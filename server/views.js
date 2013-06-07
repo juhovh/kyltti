114b11f96b1587ec729e58580f0dfb9317b26b9d
Juho Vähä-Herttua <juho.vaha-herttua@futurice.com>
2013-06-07 17:04:02 +0300

    Show selected group in the menu

--- server/views.js
+++ server/views.js
@@ -2,32 +2,49 @@ var _ = require('underscore');
 
 var db = require('./db');
 
+function getSortedGroups(selected, callback) {
+  if (typeof(selected) === 'function') {
+    callback = selected;
+    selected = null;
+  }
+  db.listGroups(function(err, rows) {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    var sortedgroups = _.sortBy(rows, function(group) { return group.name; });
+    var groups = _.map(sortedgroups, function(group) {
+      var random_idx = Math.floor((Math.random()*group.photo_ids.length));
+      return {
+        name: group.name,
+        url: '/groups/'+group.id,
+        imageurl: '/api/photos/'+group.photo_ids[random_idx]+'/thumb',
+        selected: (selected === ''+group.id)
+      };
+    });
+    callback(null, groups);
+  });
+}
+
 exports.setup = function(app) {
 
   app.get('/', function(req, res) {
-    db.listGroups(function(err, rows) {
+    getSortedGroups(function(err, groups) {
       if (err) {
         res.send(500);
         return;
       }
 
-      var sortedgroups = _.sortBy(rows, function(group) { return group.name; });
-      var groups = _.map(sortedgroups, function(group) {
-        return {
-          url: '/groups/'+group.id,
-          name: group.name
-        };
-      });
-      var thumbnails = _.map(sortedgroups, function(group) {
-        var random_idx = Math.floor((Math.random()*group.photo_ids.length));
-        return {
-          url: '/groups/'+group.id,
-          imageurl: '/api/photos/'+group.photo_ids[random_idx]+'/thumb',
-          caption: group.name
-        };
-      });
       res.render('menu', {groups: groups}, function(err, menu) {
-        res.render('imagegrid', {thumbnails: thumbnails}, function(err, imagegrid) {
+        if (err) {
+          res.send(500);
+          return;
+        }
+        res.render('imagegrid', {groups: groups}, function(err, imagegrid) {
+          if (err) {
+            res.send(500);
+            return;
+          }
           res.render('main', {
             menu: menu,
             content: imagegrid
@@ -38,27 +55,24 @@ exports.setup = function(app) {
   });
 
   app.get('/groups/:id', function(req, res) {
-    db.listGroups(function(err, groups) {
+    getSortedGroups(req.params.id, function(err, groups) {
+      if (err) {
+        res.send(500);
+        return;
+      }
       db.findPhotosByGroupId(req.params.id, function(err, photos) {
         if (err) {
           res.send(500);
           return;
         }
 
-        var sortedgroups = _.sortBy(groups, function(group) { return group.name; });
-        var groupmodels = _.map(sortedgroups, function(group) {
-          return {
-            url: '/groups/'+group.id,
-            name: group.name
-          };
-        });
         var imagemodels = _.map(photos, function(photo) {
           return {
             url: '/api/photos/'+photo.id+'/medium',
             description: photo.description
           };
         });
-        res.render('menu', {groups: groupmodels}, function(err, menu) {
+        res.render('menu', {groups: groups}, function(err, menu) {
           res.render('imagelist', {images: imagemodels}, function(err, imagelist) {
             res.render('main', {
               menu: menu,
